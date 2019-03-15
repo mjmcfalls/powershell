288c3457b7c3dd02820fffc2aaa2f5808b6e07e9
Matthew McFalls <github@msquare.33mail.com>
2019-03-14 20:00:52 -0400

    Update checkFilesForInvalidChars.ps1
    Commenting out checking for trailing spaces, because it triggers on every file.

--- checkFilesForInvalidChars.ps1
+++ checkFilesForInvalidChars.ps1
@@ -1,31 +1,44 @@
-param([String]$path=$null)
+param([String]$path = $null)
 
 $invalidChars = [System.IO.Path]::GetInvalidFileNameChars() -join ''
 
 $invalidCharsRegex = [regex]::Escape($invalidChars) 
-$trailingSpaceRegex = [regex]::new("(?<=\S)\s+$")
+# $trailingSpaceRegex = [regex]::new("\s+$")
 
-if( -not $path){
+if ( -not $path) {
     throw "No Path specified!"
 }
 
+Write-Host "Checking files and folders in $($path)."
+try {
+    $files = Get-ChildItem -Path $path -Recurse 2> $outnull
+    if (-not $?) {
+        #whatever action you want to perform
+        $msg = $msg + "Error accessing " + $dir + ": " + $error[0].Exception.Message
+    }
 
-Get-ChildItem -Path c:\ -Recurse | ForEach-Object{
+}
+catch {
+    $msg = "Error accessing " + $dir + ": " + $_.Exception.Message
+}
+
+
+$files | ForEach-Object {
     $name = $_.Name
     $rename = $false
     # Write-Host "Checking $($name)"
-    if($name.contains($invalidCharsRegex)){
+    if ($name.contains($invalidCharsRegex)) {
         Write-Host "$($name) contains invalid charaters"
         $name = $name -replace $invalidCharsRegex
         $rename = $true 
     }
 
-    if($name.contains($trailingSpaceRegex)){
-        Write-Host "$($name) contains trailing space."
-        $name = $name -replace $trailingSpaceRegex
-        $rename = $true 
-    }
-    if($rename){
+    # if ($name.contains($trailingSpaceRegex)) {
+    #     Write-Host "$($name) contains trailing space."
+    #     $name = $name -replace $trailingSpaceRegex
+    #     $rename = $true 
+    # }
+    if ($rename) {
         Write-Host "Renaming $($_.FullName) to $($name)"
         # Rename-Item -Path $_.FullName -Newname $name
     }
