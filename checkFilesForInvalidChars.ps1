85ef11ae461f52dcf318d2cbd4c1b039993ce782
Matthew McFalls <github@msquare.33mail.com>
2019-03-19 19:43:30 -0400

    Fix: Add directory checking
    Adding directory check, and changed to pull root folders, then process any directories inside instead of pulling all the files/directories in one shot.

--- checkFilesForInvalidChars.ps1
+++ checkFilesForInvalidChars.ps1
@@ -4,14 +4,15 @@ $invalidChars = [System.IO.Path]::GetInvalidFileNameChars() -join ''
 
 $invalidCharsRegex = [regex]::Escape($invalidChars) 
 # $trailingSpaceRegex = [regex]::new("\s+$")
+[regex]$dirRegex = '[dD](irectory)'
 
 if ( -not $path) {
     throw "No Path specified!"
 }
 
-Write-Host "Checking files and folders in $($path)."
+Write-Host "Getting root directores in $($path)"
 try {
-    $files = Get-ChildItem -Path $path -Recurse 2> $outnull
+    $directories = Get-ChildItem -Path $path 2> $outnull
     if (-not $?) {
         #whatever action you want to perform
         $msg = $msg + "Error accessing " + $dir + ": " + $error[0].Exception.Message
@@ -23,24 +24,36 @@ catch {
 }
 
 
-$files | ForEach-Object {
-    $name = $_.Name
-    $rename = $false
-    # Write-Host "Checking $($name)"
-    if ($name.contains($invalidCharsRegex)) {
-        Write-Host "$($name) contains invalid charaters"
-        $name = $name -replace $invalidCharsRegex
-        $rename = $true 
+$directories | Sort-Object | ForEach-Object {
+    Write-Host "Checking $($_.FullName)"
+    if ($dirRegex.Match($_.Attributes)) {
+        $files = Get-ChildItem -Path $_.FullName -Recurse
+        Foreach ($file in $files) {
+            $name = $file.Name
+            $rename = $false
+            # Write-Host "Checking $($name)"
+            if ($name.contains($invalidCharsRegex)) {
+                Write-Host "$($name) contains invalid characters"
+                # $name = $name -replace $invalidCharsRegex
+                $rename = $true 
+            }
+        
+            # if ($name.contains($trailingSpaceRegex)) {
+            #     Write-Host "$($name) contains trailing space."
+            #     $name = $name -replace $trailingSpaceRegex
+            #     $rename = $true 
+            # }
+            if ($rename) {
+                Write-Host "Renaming $($file.FullName) to $($name)"
+                # Rename-Item -Path $_.FullName -Newname $name
+            }
+        }
     }
-
-    # if ($name.contains($trailingSpaceRegex)) {
-    #     Write-Host "$($name) contains trailing space."
-    #     $name = $name -replace $trailingSpaceRegex
-    #     $rename = $true 
-    # }
-    if ($rename) {
-        Write-Host "Renaming $($_.FullName) to $($name)"
-        # Rename-Item -Path $_.FullName -Newname $name
+    else {
+        if ($_.Name.contains($invalidCharsRegex)) {
+            Write-Host "$($name) contains invalid characters"
+            # $name = $name -replace $invalidCharsRegex
+            $rename = $true 
+        }
     }
-    
 }
