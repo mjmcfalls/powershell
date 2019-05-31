31f2d91c15e4ec735e2b63c8292f03617b3661f3
Matthew McFalls <github@msquare.33mail.com>
2019-05-31 10:25:51 -0400

    Fix: Change ForEach to ForEach-Object
    Change ForEach to ForEach-Object to remove alias for maintainability.

--- clearOldFilesFromTarget.ps1
+++ clearOldFilesFromTarget.ps1
@@ -79,7 +79,7 @@ Function Clean-Dirs {
             # Write-Host 'Try getting child dirs.'
             Get-ChildItem -LiteralPath "$($Path)" -Force -Directory -Recurse -ErrorAction "Stop" | 
             Sort-Object -Property @{Expression = "FullName"; Descending = $True } |
-            ForEach {
+            ForEach-Object {
                 Write-Host "Dir: $($_.FullName)"
                 Remove-LongPathNames $_.FullName
             }
@@ -89,7 +89,7 @@ Function Clean-Dirs {
             $Path = "\\?\UNC" + $Path.Substring(1)
             Get-ChildItem -LiteralPath "$($Path)" -Force -Directory -Recurse | 
             Sort-Object -Property @{Expression = "FullName"; Descending = $True } |
-            ForEach {
+            ForEach-Object {
                 Write-Host "Dir: $($_.FullName)"
                 Remove-LongPathNames $_.FullName
             }
@@ -102,7 +102,7 @@ Function Clean-Dirs {
             Get-ChildItem -LiteralPath "$($Path)" -Force -Recurse -File -ErrorAction "Stop" |
             Where-Object { -Not $_.PSIsContainer } | 
             Sort-Object -Property  @{Expression = "FullName"; Descending = $True } |
-            ForEach {
+            ForEach-Object {
                 If (-Not $_.PSIsContainer) {
                     # Remove Files
                     Write-host "File: $($_.FullName)"
@@ -116,7 +116,7 @@ Function Clean-Dirs {
             Get-ChildItem -LiteralPath "$($Path)" -Force -File -Recurse |
             Where-Object { -Not $_.PSIsContainer } | 
             Sort-Object -Property  @{Expression = "FullName"; Descending = $True } |
-            ForEach {
+            ForEach-Object-Object {
                 If (-Not $_.PSIsContainer) {
                     # Remove Files
                     Write-host "File: $($_.FullName)"
@@ -135,7 +135,7 @@ $CountRemovedDirs = 0
 Get-ChildItem -Path $Path -Force -Directory | 
 Where-Object { $_.LastWriteTime -lt $TargetDate } | 
 Sort-Object -Property @{Expression = "PSIsContainer"; Descending = $False }, @{Expression = "FullName"; Descending = $False } |
-ForEach {
+ForEach-Object {
     Clean-Dirs -Path $_.FullName
     Clean-Dirs -Path $_.FullName -Directory
     $CountRemovedDirs += 1
